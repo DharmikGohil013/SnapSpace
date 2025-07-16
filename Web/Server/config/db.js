46d8172982781b736a5cc3570686e2f7882885be
DharmikGohil013 <dharmikgohil395003@gmail.com>
2025-07-16 23:56:30 +0530

    sever full adeed

--- Web/Server/config/db.js
+++ Web/Server/config/db.js
@@ -1,16 +1,15 @@
+// config/db.js
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`🟢 MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error(`❌ MongoDB Connection Error: ${error.message}`);
+    process.exit(1); // Exit app if DB fails
   }
 };
 
